62dc9f44e7d162d0aace7230940fe4edfa3db29f
encorehu <huyoo353@126.com>
2014-05-07 11:26:11 +0800

    add is_what...

---
+++ validators/__init__.py
@@ -0,0 +1,32 @@
+def is_string(value):
+    return False
+
+def is_integer(value):
+    return False
+
+def is_number(value):
+    return False
+
+def is_email(value):
+    return False
+
+def is_url(value):
+    return False
+
+def is_ip(value):
+    return False
+
+def is_ipv4(value):
+    return False
+
+def is_ipv6(value):
+    return False
+
+def is_domain(value):
+    return False
+
+def is_cellphone(value):
+    return False
+
+def is_telephone(value):
+    return False
