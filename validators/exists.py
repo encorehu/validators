ac9775949297a70187fa72b99c7b78463f64c83e
encorehu <huyoo353@126.com>
2014-05-07 11:27:12 +0800

    add exists.py

---
+++ validators/exists.py
@@ -0,0 +1,21 @@
+def url_exists(target, source=None):
+    return False
+
+def title_exists(target, source=None):
+    return False
+
+def ip_exists(target, source=None):
+    return False
+
+def domain_exists(target, source=None):
+    return False
+
+def email_exists(target, source=None):
+    return False
+
+def cellphone_exists(target, source=None):
+    return False
+
+def telephone_exists(target, source=None):
+    return False
+
